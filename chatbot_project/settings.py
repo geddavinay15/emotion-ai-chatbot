38e0ad12c9777090309b08e6e0a7454977b94938
Vinay Sankar <geddavinaysankar@gmail.com>
2026-05-28 13:32:23 +0530

    Prepared project for deployment

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dlxbuzm%=dv#p#*en8@g-2o8a_-9@6)tr$ln^=nrh**q9ka2k2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
